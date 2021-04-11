f89078d8d6ba74261865128557b7d262d93a44f4
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-04-11 10:56:50 -0600

    Update active players method

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -7,20 +7,45 @@ class WorldCup
     @teams = teams
   end
 
-  def eliminated
-    binding.pry
-    @teams.eliminated
+  def midfielders
+    players = {}
+
+    @teams.each do |team|
+      team.players.each do |player|
+        if players[player.position]
+          players[player.position].push(player)
+        else
+          players[player.position] = [player]
+        end
+      end
+    end
+    players["midfielder"]
   end
 
+
   def active_players_by_position(position)
-    players = teams.map do |team|
-      if eliminated == false
-        # binding.pry
-        team.players
+    players = []
+    @teams.flat_map do |team|
+        team.players.flat_map do |player|
+          if player.position == position
+            players.push(player)
+            # binding.pry
+          elsif !team.eliminated?
+            players[0]
+          end
+          binding.pry
+        end.compact.uniq
       end
-    end.flatten
-    players.find_all do |player|
-      player.position == position
     end
+    # players
   end
-end
+
+# def active_players_by_position(position)
+#   players = teams.map do |team|
+#       team.players
+#     end
+#   end.flatten
+#   players.find_all do |player|
+#     player.position == position
+#   end
+# end
--- spec/world_cup_spec.rb
+++ spec/world_cup_spec.rb
@@ -4,7 +4,7 @@ require './lib/player'
 require './lib/team'
 
 RSpec.describe WorldCup do
-  it 'exists' do
+  xit 'exists' do
     france = Team.new("France")
     mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
     pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
@@ -21,7 +21,7 @@ RSpec.describe WorldCup do
     expect(world_cup).to be_instance_of(WorldCup)
   end
 
-  it 'has a year' do
+  xit 'has a year' do
     france = Team.new("France")
     mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
     pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
@@ -38,7 +38,7 @@ RSpec.describe WorldCup do
     expect(world_cup.year).to eq(2018)
   end
 
-  it 'has teams' do
+  xit 'has teams' do
     france = Team.new("France")
     mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
     pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
@@ -55,7 +55,7 @@ RSpec.describe WorldCup do
     expect(world_cup.teams).to eq([france, croatia])
   end
 
-  it 'has active players by position' do
+  it 'has active players by position with a team being eliminated' do
     france = Team.new("France")
     mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
     pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
@@ -70,24 +70,8 @@ RSpec.describe WorldCup do
     world_cup = WorldCup.new(2018, [france, croatia])
 
     expect(world_cup.active_players_by_position("midfielder")).to eq([pogba, modric])
-  end
 
-  it 'has active players by position with a team being eliminated' do
-    france = Team.new("France")
-    mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
-    pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
-    france.add_player(mbappe)
-    france.add_player(pogba)
-
-    croatia = Team.new("Croatia")
-    modric = Player.new({name: "Luka Modric", position: "midfielder"})
-    vida = Player.new({name: "Domagoj Vida", position: "defender"})
-    croatia.add_player(modric)
-    croatia.add_player(vida)
-    world_cup = WorldCup.new(2018, [france, croatia])
     croatia.eliminated = true
-    # binding.pry
-    croatia.eliminated?
 
     expect(world_cup.active_players_by_position("midfielder")).to eq([pogba])
   end
